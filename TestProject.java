37c939799d4065252bbbd8f87327f36ea9eeed88
maxvons <43211032+maxvons@users.noreply.github.com>
2019-10-08 16:54:09 +0200

    Update TestProject.java

--- TestProject.java
+++ TestProject.java
@@ -1,39 +1,56 @@
+import java.util.LinkedList;
+import java.util.Stack;
+import java.util.Queue;
+
 class TestProject {
     public static void main(String[] args) {
         Project p = Project.buildProject("buildhouse1.txt");
         p.addOutEdges();
+        p.addPredecessors();
         p.printTasks();
         p.printOutEdges();
         // p.topologicalSort();
         
         // Test without cycle.
         p.isRealizable();
-        p.resetVisited();
-        p.resetActive();
+        Queue<Task> test = p.topologicalSort();
+        p.addEarliestStart(test);
+        p.printSortedTasks(test);
+        
+        p.printTimeSchedule(test, p.getLastStart());
 
         // Test with more advanced cycle
         // p.addEdge(4, 3);
         // p.addEdge(3, 4);
         // System.out.println("\n\nADVANCED CYCLE");
-        // p.isRealizable();
+        // // p.isRealizable();
         // p.resetActive();
         // p.resetVisited();
+        // p.fillWhiteSet();
+        // p.findCycle(p.getFirstTask());
+        // p.checkCodependency(p.getTasks());
         
 
         // Test with cycle
         // p.addEdge(3, 1);
         // p.isRealizable();
-        // p.resetVisited();
-        // p.resetActive();
-        // Test with cycle
-        p.addEdge(3, 5);
-        p.isRealizable();
-        p.resetVisited();
-        p.resetActive();
+        // p.fillWhiteSet();
+        // p.findCycle(p.getFirstTask());
+        // Queue<Task> test = p.topologicalSort();
 
+
+        // Test with cycle
+        // p.fillWhiteSet();
+        // p.addEdge(3, 5);
+        // p.isRealizable();
+        // p.findCycle(p.getFirstTask());
+        // Queue<Task> test = p.topologicalSort();
         // Test with another cycle
         // p.addEdge(8, 7);
         // p.addDependencyEdge(7, 8);
+        // p.checkCodependency(p.getTasks());
         // p.isRealizable();
+        // p.fillWhiteSet();
+        // p.findCycle(p.getFirstTask());
     }
 }
